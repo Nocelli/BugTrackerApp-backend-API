f8831317907b41dcdd9f970a2e21598425778d94
Nocelli <rafaelnocelliifsp@gmail.com>
2020-04-08 23:31:44 -0300

    Changes to cors configurations to expose headers

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const routes = require('./routes.js')
 const { errors } = require('celebrate')
 const cors = require('cors')
 
+const corsOptions = {
+    exposedHeaders: 'x-token, x-token-refresh',
+  }
+
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(routes)
 app.use(errors())
